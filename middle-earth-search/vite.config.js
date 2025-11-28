353af94f54f2784ebb34f2d06af0c88d3e09ecf5
Robert McElhinney <robmcelhinney@hotmail.com>
2025-11-28 21:09:26 +0000

    ghpages old way

--- middle-earth-search/vite.config.js
+++ middle-earth-search/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // ADD THIS LINE:
+    // Keep this for subpath deployment:
     base: "/MiddleEarthSemanticSearch/",
 })
